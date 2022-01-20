51dd659b850c4ab0f251f9eeb892e1e9a03f5658
carljoeleriksson <carljoeleriksson@gmail.com>
2022-01-20 13:17:15 +0100

    finished without testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Home from './pages/HomePage'
+import Details from './pages/MeetupDetails'
+
+import Header from './components/Header'
+
+import './App.css'
 
 function App() {
   return (
+    <div className="App">
+      <Router>
 
-  );
+        <main>
+          <Route path="/" exact component={Home} />
+          <Route path="/details/:id" component={Details} />
+        </main>
+
+        <footer>
+          &copy; Meetup-Project 2022
+        </footer>
+      </Router>
+    </div>
+  )
 }
 
-export default App;
+export default App
