59cae9c5a9b65b302550ae837997f0dccf6d1e07
carljoeleriksson <carljoeleriksson@gmail.com>
2022-01-20 15:27:26 +0100

    Added css for meetup details in App.css

--- src/pages/MeetupDetails.tsx
+++ src/pages/MeetupDetails.tsx
@@ -16,8 +16,8 @@ function MeetupDetails() {
     <div className="meetup-details-wrapper">
         <img src="https://images.unsplash.com/photo-1518609878373-06d740f60d8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80" alt="Meetup Name" />
         <button className='attend-btn' onClick={attendClick}>{!isAttended ? 'Attend' : 'Unattend'}</button>
-        <p className="details-date" data-testid="date">Monday, January 20, 2022</p>
-        <h3>Meetup Name</h3>
+        <p className="details-date" data-testid="date">Monday | January 20 | 2022</p>
+        <h3 className="details-title">Meetup Name</h3>
         <p className="details-host"><span>Host: </span>Joel Eriksson</p>
         <p className="details-desc" data-testid="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />Beatae consequatur eaque ratione provident minima eius odio facere impedit assumenda sapiente?</p>
     </div>
