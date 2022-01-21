ce42ea05930ffa2bb04e18d0a62b68712564621c
carljoeleriksson <carljoeleriksson@gmail.com>
2022-01-21 10:08:34 +0100

    started testing

---
+++ src/__tests__/Comments.test.tsx
@@ -0,0 +1,12 @@
+describe('Comments component', () => {
+    //renders without crashing
+    //doesnt show input field initially
+    //shows an icon for comments
+    //does show input filed when comment icon is clicked
+    //does not show input field when comment icon is clicked twice
+    //empties the input field when user presses the enter key
+    //empties the input field when user presses send
+    //shows message in comment-section when user writes then presses the enter key
+    //shows message in comment-section when user writes then presses send
+    
+})
