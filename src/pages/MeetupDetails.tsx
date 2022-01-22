3fbbf4b1816f0daead92c7619544bf10c20b4c1d
Omar Mh <mr.omr86@gmail.com>
2022-01-22 14:57:05 +0100

    attend/unattend meetup

--- src/pages/MeetupDetails.tsx
+++ src/pages/MeetupDetails.tsx
@@ -1,29 +1,168 @@
-import React, {useState} from 'react';
-import {FaArrowAltCircleLeft} from 'react-icons/fa'
+import React, { useEffect, useState } from 'react';
+import { FaArrowAltCircleLeft } from 'react-icons/fa'
 
-function MeetupDetails() {
+
+function MeetupDetails(props: any) {
+
     const [isAttended, setIsAttended] = useState<boolean>(false)
+
+    const [meetUp, setMeetUp]: Array<any> = useState([])
+
+    var meetupListArr: Array<any> = useState([])
+
+    var singleMeetup: Array<any> = []
+
+    var meetupId: any = null
+
+    const search = window.location.search;
+
+    const params = new URLSearchParams(search);
+
+
+    function getMeetupIdFromUrl() {
+
+        meetupId = params.get('id');
+
+        if (!meetupId)
+
+            throw new Error("Meetup Id is missing!")
+
+        console.log(meetupId)
+
+
+    }
+
     function attendClick() {
-        if(!isAttended){
-            setIsAttended(true)
-        } else {
-            setIsAttended(false)
+
+
+        let Attend = isAttended ? false : true
+
+        try {
+
+            setAttendedToLocalStorage(Attend)
+
+            setIsAttended(Attend)
+
+        } catch (e) {
+
+            console.log(e)
+
         }
     }
 
-  return <>
-    <button className='back-btn icon-btn'><FaArrowAltCircleLeft/>Back</button>
-    <div className="meetup-details-wrapper">
-        <img src="https://images.unsplash.com/photo-1518609878373-06d740f60d8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80" alt="Meetup Name" />
-        <button className='attend-btn' onClick={attendClick}>{!isAttended ? 'Attend' : 'Unattend'}</button>
-        <p className="details-date" data-testid="date">Monday | January 20 | 2022</p>
-        <h3 className="details-title">Meetup Name</h3>
-        <p className="details-host"><span>Host: </span>Joel Eriksson</p>
-        <p className="details-desc" data-testid="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />Beatae consequatur eaque ratione provident minima eius odio facere impedit assumenda sapiente?</p>
-    </div>
-    
-    {/* Insert <Comments/> here */}
-  </>
+    function setAttendedToLocalStorage(attended: boolean) {
+
+        if (meetupListArr.length == 0)
+
+            throw new Error("can not find the meetup list")
+
+        let updatedMeetupListArr = meetupListArr.map((el: any) => {
+
+            if (el.Id == meetupId) {
+                
+                el.Attend = attended
+            }
+            return el
+        })
+
+        console.log(updatedMeetupListArr)
+
+
+        localStorage.setItem('meetUp-List', JSON.stringify(updatedMeetupListArr))
+
+    }
+
+    function getMeetupListfmLocalStorage() {
+
+        let meetupListStr: string | null = localStorage.getItem('meetUp-List')
+
+        if (!meetupListStr) {
+            throw new Error('can not find meetup List in Localstorage ')
+        }
+
+        let meetupList: object = JSON.parse(meetupListStr)
+
+        console.log(meetupList)
+
+        meetupListArr = Object.values(meetupList)
+
+        //  return meetupListArr
+
+    }
+
+    function getReqMeetup() {
+
+        singleMeetup = meetupListArr.filter((meetup: any) => {
+
+            return meetup.Id == meetupId
+        }
+
+        )
+        console.log(singleMeetup)
+
+        // return singleMeetup
+    }
+
+
+
+    function updateMeetupState() {
+
+        setMeetUp(singleMeetup)
+
+    }
+
+    function updateAttendState() {
+
+        setIsAttended(singleMeetup[0].Attend)
+
+    }
+
+
+    getMeetupIdFromUrl()
+
+    getMeetupListfmLocalStorage()
+
+    getReqMeetup()
+
+
+
+    useEffect(() => {
+
+        try {
+
+            updateMeetupState()
+
+            updateAttendState()
+
+        } catch (e) {
+
+            console.log(e)
+        }
+
+    }, [])
+
+
+    return <>
+        <button className='back-btn icon-btn'><FaArrowAltCircleLeft />Back</button>
+
+        {meetUp.length > 0 && meetUp.map((el: any) => (
+
+            <div key={el.Id} className="meetup-details-wrapper">
+                <img src={el.Image} alt={el.Title} />
+                <button className='attend-btn' onClick={attendClick}>{!isAttended ? 'Attend' : 'Unattend'}</button>
+                <p className="details-date" data-testid="date">{el.Date}</p>
+                <h3 className="details-title">{el.Title}</h3>
+                <p className="details-host"><span>Host: </span>{el.Host}</p>
+                <p className="details-desc" data-testid="description">{el.Description}</p>
+            </div>
+        )
+        )
+        }
+
+        {/* Insert <Comments/> here */}
+    </>
 }
 
 export default MeetupDetails;
+
+
