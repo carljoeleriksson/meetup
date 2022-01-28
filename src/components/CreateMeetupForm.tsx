00f7532282da380c2ad50ffe93d8133b944ad8e9
carljoeleriksson <carljoeleriksson@gmail.com>
2022-01-28 11:03:35 +0100

    WIP creating tsx and test files

--- src/components/CreateMeetupForm.tsx
+++ src/components/CreateMeetupForm.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
+function todaysDate() {
+  const current = new Date();
+  const date = `${current.getFullYear()}-${current.getMonth()+1}-${current.getDate()}`;
+  return date
+}
+
 function CreateMeetupForm() {
-  return <>
+    return <>
         <h2>Create Meetup</h2>
-  </>
+        <form >
+            <label htmlFor="date-input">Date</label>
+            <input 
+                id="date-input" 
+                type="date" 
+                value={todaysDate()}
+            />
+        </form>
+    
+    
+    </>
 }
 
 export default CreateMeetupForm
