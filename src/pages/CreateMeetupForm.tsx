439ba3164474b9a337ba6ce351040804e3d1143e
carljoeleriksson <carljoeleriksson@gmail.com>
2022-01-31 14:28:19 +0100

    Added Back-btn from createMeetupForm

--- src/pages/CreateMeetupForm.tsx
+++ src/pages/CreateMeetupForm.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
+import { FaArrowAltCircleLeft } from 'react-icons/fa'
 
 function todaysDate() {
   const current = new Date();
@@ -51,54 +52,56 @@ function CreateMeetupForm() {
     }
 
     return <>
+        
         <h2>Create Meetup</h2>
         <div className='create-meetup-wrapper'>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="title-input">Title</label>
-            <input 
-                id="title-input"
-                name="title" 
-                type="text" 
-            />
-            <label htmlFor="date-input">Date</label>
-            <input 
-                id="date-input" 
-                name="date"
-                type="date" 
-                placeholder={todaysDate()}
-            />
-            <label htmlFor="time-input">Time</label>
-            <input 
-                id="time-input"
-                name="time" 
-                type="time"
-            />
-            <label htmlFor="desc-input">Description</label>
-            <input 
-                id="desc-input" 
-                name="description"
-                type="textarea" 
-            />
-            <label htmlFor="host-input">Host</label>
-            <input 
-                id="host-input" 
-                name="host"
-                type="text" 
-            />
-            <label htmlFor="cat-input">Category</label>
-            <input 
-                id="cat-input" 
-                name="category"
-                type="text" 
-            />
-            <label htmlFor="img-input">Image</label>
-            <input 
-                id="img-input" 
-                name="image"
-                type="text" 
-            />
-            <button className='create-btn' type="submit">Create Meetup</button>   
-        </form>    
+            <Link to="/" className='back-btn icon-btn'><FaArrowAltCircleLeft />Back</Link>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="title-input">Title</label>
+                <input 
+                    id="title-input"
+                    name="title" 
+                    type="text" 
+                />
+                <label htmlFor="date-input">Date</label>
+                <input 
+                    id="date-input" 
+                    name="date"
+                    type="date" 
+                    placeholder={todaysDate()}
+                />
+                <label htmlFor="time-input">Time</label>
+                <input 
+                    id="time-input"
+                    name="time" 
+                    type="time"
+                />
+                <label htmlFor="desc-input">Description</label>
+                <input 
+                    id="desc-input" 
+                    name="description"
+                    type="textarea" 
+                />
+                <label htmlFor="host-input">Host</label>
+                <input 
+                    id="host-input" 
+                    name="host"
+                    type="text" 
+                />
+                <label htmlFor="cat-input">Category</label>
+                <input 
+                    id="cat-input" 
+                    name="category"
+                    type="text" 
+                />
+                <label htmlFor="img-input">Image</label>
+                <input 
+                    id="img-input" 
+                    name="image"
+                    type="text" 
+                />
+                <button className='create-btn' type="submit">Create Meetup</button>   
+            </form>    
         </div>
     </>
 }
